c1f56be54e581f9a4916f7bd5a516b90935d1348
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-07-22 22:42:30 +0200

    Add products

---
+++ util/data.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    description: 'This is product 1',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200,
+    description: 'This is product 2',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300,
+    description: 'This is product 3',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    price: 400,
+    description: 'This is product 4',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 5,
+    name: 'Product 5',
+    price: 500,
+    description: 'This is product 5',
+    image: 'https://picsum.photos/200/300',
+  },
+];
