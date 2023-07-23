befd1b2ce472fce59750c0b627a3c555ccd3bc64
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-07-24 01:07:23 +0200

    Rename app and add version

--- android/app/src/main/java/com/volavid/marketplace/MainApplication.java
+++ android/app/src/main/java/com/volavid/marketplace/MainApplication.java
@@ -1,4 +1,4 @@
-package com.firstnativeapp;
+package com.volavid.marketplace;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/VolavidMarketplace/AppDelegate.mm
+++ ios/VolavidMarketplace/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"FirstNativeApp";
+  self.moduleName = @"Volavid Marketplace";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
