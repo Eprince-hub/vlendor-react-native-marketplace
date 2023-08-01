b7091cf1b516096a2e4b99f2d07ff28c1972fcee
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-01 22:49:30 +0200

    Modify data structure

--- util/data.ts
+++ util/data.ts
@@ -1,37 +1,62 @@
-export const products = [
+export const services = [
   {
     id: 1,
-    name: 'Product 1',
+    name: 'Service 1',
     price: 100,
-    description: 'This is product 1',
+    description: 'This is service 1',
     image: 'https://picsum.photos/200/300',
+    serviceProvider: {
+      id: 1,
+      name: 'Provider 1',
+      image: 'https://picsum.photos/200/300',
+    },
   },
   {
     id: 2,
-    name: 'Product 2',
+    name: 'Service 2',
     price: 200,
-    description: 'This is product 2',
+    description: 'This is service 2',
     image: 'https://picsum.photos/200/300',
+    serviceProvider: {
+      id: 2,
+      name: 'Provider 2',
+      image: 'https://picsum.photos/200/300',
+    },
   },
   {
     id: 3,
-    name: 'Product 3',
+    name: 'Service 3',
     price: 300,
-    description: 'This is product 3',
+    description: 'This is service 3',
     image: 'https://picsum.photos/200/300',
+    serviceProvider: {
+      id: 3,
+      name: 'Provider 3',
+      image: 'https://picsum.photos/200/300',
+    },
   },
   {
     id: 4,
-    name: 'Product 4',
+    name: 'Service 4',
     price: 400,
-    description: 'This is product 4',
+    description: 'This is service 4',
     image: 'https://picsum.photos/200/300',
+    serviceProvider: {
+      id: 4,
+      name: 'Provider 4',
+      image: 'https://picsum.photos/200/300',
+    },
   },
   {
     id: 5,
-    name: 'Product 5',
+    name: 'Service 5',
     price: 500,
-    description: 'This is product 5',
+    description: 'This is service 5',
     image: 'https://picsum.photos/200/300',
+    serviceProvider: {
+      id: 5,
+      name: 'Provider 5',
+      image: 'https://picsum.photos/200/300',
+    },
   },
 ];
