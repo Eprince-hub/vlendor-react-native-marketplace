44858ab8750f65e9c869f8855187bd7320fccf27
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-02 01:47:42 +0200

    Refactor data.ts file

--- util/data.ts
+++ util/data.ts
@@ -4,11 +4,11 @@ export const services = [
     name: 'Service 1',
     price: 100,
     description: 'This is service 1',
-    image: 'https://picsum.photos/200/300',
+    image: '../assets/productImages/cleaning.jpg',
     serviceProvider: {
       id: 1,
       name: 'Provider 1',
-      image: 'https://picsum.photos/200/300',
+      image: '../assets/productImages/cleaning.jpg',
     },
   },
   {
@@ -16,11 +16,11 @@ export const services = [
     name: 'Service 2',
     price: 200,
     description: 'This is service 2',
-    image: 'https://picsum.photos/200/300',
+    image: '../assets/productImages/cleaning.jpg',
     serviceProvider: {
       id: 2,
       name: 'Provider 2',
-      image: 'https://picsum.photos/200/300',
+      image: '../assets/productImages/cleaning.jpg',
     },
   },
   {
@@ -28,11 +28,11 @@ export const services = [
     name: 'Service 3',
     price: 300,
     description: 'This is service 3',
-    image: 'https://picsum.photos/200/300',
+    image: '../assets/productImages/cleaning.jpg',
     serviceProvider: {
       id: 3,
       name: 'Provider 3',
-      image: 'https://picsum.photos/200/300',
+      image: '../assets/productImages/cleaning.jpg',
     },
   },
   {
@@ -40,11 +40,11 @@ export const services = [
     name: 'Service 4',
     price: 400,
     description: 'This is service 4',
-    image: 'https://picsum.photos/200/300',
+    image: '../assets/productImages/cleaning.jpg',
     serviceProvider: {
       id: 4,
       name: 'Provider 4',
-      image: 'https://picsum.photos/200/300',
+      image: '../assets/productImages/cleaning.jpg',
     },
   },
   {
@@ -52,11 +52,20 @@ export const services = [
     name: 'Service 5',
     price: 500,
     description: 'This is service 5',
-    image: 'https://picsum.photos/200/300',
+    image: '../assets/productImages/cleaning.jpg',
     serviceProvider: {
       id: 5,
       name: 'Provider 5',
-      image: 'https://picsum.photos/200/300',
+      image: '../assets/productImages/cleaning.jpg',
     },
   },
 ];
+
+export function getProducts() {
+  console.log('services', services);
+  return services;
+}
+
+export function getProduct(id: number) {
+  return services.find(service => service.id === id);
+}
