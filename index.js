87e0b4c34eb5d7337e2ac28d0a113cade99e4ad1
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-08 23:33:14 +0200

    Add theme provider wrapper

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ import {NavigationContainer} from '@react-navigation/native';
  */
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import {PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {name as appName} from './app.json';
+import {ThemeProvider} from './components/providers/ThemeProvider';
 import App from './src/App';
 
 const AppComponent = () => {
   return (
-    <PaperProvider>
+    <ThemeProvider>
       <SafeAreaProvider>
         <NavigationContainer>
           <App />
         </NavigationContainer>
       </SafeAreaProvider>
-    </PaperProvider>
+    </ThemeProvider>
   );
 };
 
