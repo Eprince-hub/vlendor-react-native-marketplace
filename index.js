409af7a83e0f7711fdd7a51134758ff80bbd8880
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-10 22:34:25 +0200

    Refactor index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import {NavigationContainer} from '@react-navigation/native';
 /**
  * @format
  */
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {name as appName} from './app.json';
 import {ThemeProvider} from './components/providers/ThemeProvider';
 import App from './src/App';
@@ -12,11 +10,7 @@ import App from './src/App';
 const AppComponent = () => {
   return (
     <ThemeProvider>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <App />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <App />
     </ThemeProvider>
   );
 };
