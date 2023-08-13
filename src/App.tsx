cd1d96094f3631ef34da797476d3abccff6a5ab5
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-13 23:43:19 +0200

    Solve react calendar error

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {en, registerTranslation} from 'react-native-paper-dates';
 import AppOnboardingNavigation from '../navigations/AppOnboardingNavigator';
 
 const stack = createNativeStackNavigator();
 
 function App() {
+  React.useEffect(() => {
+    registerTranslation('en', en);
+  }, []);
   return (
     <stack.Navigator initialRouteName="OnboardingScreen">
       <stack.Screen
