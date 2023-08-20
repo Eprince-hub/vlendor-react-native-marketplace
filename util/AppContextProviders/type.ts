773b294372b574499730ed132c3663b5376c26b4
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-21 00:35:48 +0200

    Add system preference type

--- util/AppContextProviders/type.ts
+++ util/AppContextProviders/type.ts
@@ -1,14 +1,21 @@
-import {SET_THEME_MODE} from './ThemeContextProvider/themeActions';
+import {
+  SET_SYSTEM_PREFERENCE,
+  SET_THEME_MODE,
+} from './ThemeContextProvider/themeActions';
 import {SET_USER_PROFILE} from './UserContextProvider/userActions';
 
-export type ThemeMode = 'light' | 'dark' | 'system';
+export type ThemeMode = 'dark' | 'light';
+export type UseSystemPreference = 'system' | null;
 
 // Actions
-export type ThemeAction = {type: typeof SET_THEME_MODE; payload: ThemeMode};
+export type ThemeAction =
+  | {type: typeof SET_THEME_MODE; payload: ThemeMode}
+  | {type: typeof SET_SYSTEM_PREFERENCE; payload: UseSystemPreference};
 
 // Replaces AppState for the initial app state
 export type ThemeInitialState = {
   themeMode: ThemeMode;
+  useSystemPreference: UseSystemPreference;
 };
 
 // initial state type
