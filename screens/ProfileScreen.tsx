bc1e3ff0c93392212472a8885677d5fbe9d528b0
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-08-23 10:01:16 +0200

    Remove unwanted imports and use concise logic

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,19 +1,22 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
-import DarkModeSwitch from '../components/darkModeComponents/DarkModeSwitch';
-import SystemPreferenceSwitch from '../components/darkModeComponents/SystemPreferenceSwitch';
-import {useTheme} from '../components/providers/ThemeProvider';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+import ThemeSwitcher from '../components/ThemeSwitcher';
+import {useAppContext} from '../util/AppContextProviders';
 import isUserLoggedIn from '../util/auth/user';
 
 export default function ProfileScreen({route}: any) {
   const navigation = useNavigation<any>();
   // Temporary variable to hold the name of the user
   const profileParams = route.params?.name || 'Guest';
-  const {themeMode, theme} = useTheme();
+
+  const {userState, themeState} = useAppContext();
+
+  console.log('ProfileScreen User Stats: ', userState.userProfile);
 
   useEffect(() => {
-    const userLoggedIn = isUserLoggedIn('');
+    const userLoggedIn = isUserLoggedIn(userState.userProfile?.name);
     console.log('User is logged in: ', userLoggedIn);
 
     if (!userLoggedIn) {
@@ -21,22 +24,17 @@ export default function ProfileScreen({route}: any) {
       // This is the code to navigate to the login screen
       navigation.navigate('LoginScreen');
     }
-  }, [navigation]);
+  }, [navigation, userState.userProfile?.name]);
 
   return (
-    <View style={{backgroundColor: theme.colors.background}}>
-      <Text style={{color: theme.colors.onBackground}}>
+    <View>
+      <Text>
         This screen will be needed for the users profile and the user related
         settings screen will be displayed here
       </Text>
-      <Text style={{color: theme.colors.onBackground}}>
-        This is {profileParams}'s profile
-      </Text>
-      <Text style={{color: theme.colors.onBackground}}>
-        My Current Theme mode is {themeMode}
-      </Text>
-      <DarkModeSwitch />
-      <SystemPreferenceSwitch />
+      <Text>This is {profileParams}'s profile</Text>
+      <Text>My Current Theme mode is {themeState.themeMode}</Text>
+      <ThemeSwitcher />
     </View>
   );
 }
